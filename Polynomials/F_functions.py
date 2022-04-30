306ef2727610dd8629cee12974d3ea02aa0c1e0b
Julien Rosenberger <julien.rosenberger@student-cs.fr>
2022-04-30 22:53:00 +0200

    F_function completed, dynamic_diff implemented. TODO: some comparison between the various method in terms of speed of differentiation

--- Polynomials/F_functions.py
+++ Polynomials/F_functions.py
@@ -1,7 +1,8 @@
 import sympy as sm
-
+import numpy as np
 
 x, y = sm.symbols("x, y")
+variables = [x, y]
 
 
 def _set_polynome_xpy_real(coords):
@@ -92,7 +93,7 @@ Definition of the function F
 class F2D():
     """
     Generates the function F on a boundary coords in ###2D### and computes values
-    of the function along with its derivatives up to 2nd order in the considered space
+    of the function along with its derivatives in the considered space
 
     Remarks:
     All expr could be set with numpy. It would simplify the script of this class.
@@ -103,6 +104,7 @@ class F2D():
         Defines the variables used to set F
         - coords: array of coordinates of boundary points (here coords.shape==(nb_points,dimension2))
         - strfn: string relating to the way we set the polynome F
+        (among : 'xpy_real', 'xpy_np_real', 'sinxpy_real', 'sinxpy_np_real', 'expxpy_real', 'expxpy_np_real', 'xpy_cmplx', 'xpy_np_cmplx')
         """
         assert len(coords)
         assert len(coords[0]) == 2
@@ -122,66 +124,81 @@ class F2D():
             strfn_no_np = l_str[0]+'_'+l_str[-1]
         else:
             strfn_no_np = strfn
-        # 2 fois la mémoire ?
+        # 2 fois la mémoire ? ###############################################################
         set_expr_fn_no_np = self.dic_strfn_to_fn[strfn_no_np]
         self.expr_no_np = set_expr_fn_no_np(coords)
         set_expr_fn = self.dic_strfn_to_fn[strfn]
         self.expr = set_expr_fn(coords)
 
-        # dico with keys of length 2 with 1st element the order of derivation wrt x, and 2nd element the order of derivation wrt y
-        # A value is the expression of the adequate derivative
-        self.dico_derivation = {}
+        # dynamic programming for differentiation
+        self.tab_diff = [[self.expr_no_np]]
+        self.reduced_tab_diff = []
 
-    # length of l_order fixed #####
+        self.max_index = 0
+        self.dico_order_to_index = {}
 
-    def derivate(self, t_order):
+    def dynamic_diff_2D(self, l_orders):
         '''
-        Return the derivative of F with respect to (variables[i]**l_order[i] for i in range(len(t_order))
-        -t_order: tuple of the orders of derivation : we derivate F wrt self.variables[i] t_order[i] times
+        differentiate F
+        Variables:
+        - l_orders : list of tuple of size 2
 
-        Remark : 
-        len(l_order)==len(self.variables)        
-        use Schwarz theorem
+        Returns:
+        - reduced_tab_diff : function or list of the expressions of the differentiates of F depending on the strfn of initialization (np or not)
         '''
-        assert len(t_order) == len(self.variables)
+        for t_order in l_orders:
+            (a, b) = t_order
+            for i in range(1, a+1):
+                if len(self.tab_diff) <= i:
+                    self.tab_diff.append([sm.diff(self.tab_diff[i-1][0], x)])
+                    print(f'differentiation of order {(i,0)} done ')
+            for j in range(1, b+1):
+                if len(self.tab_diff[a]) <= j:
+                    self.tab_diff[a].append(sm.diff(self.tab_diff[a][j-1], y))
+                    print(f'differentiation of order {(j,a)} done ')
 
-        if t_order in self.dico_derivation:
-            return self.dico_derivation[t_order]
-        else:
-            # absence d'effet de bord
-            dfn = self.expr_no_np
-            for i, var in enumerate(self.variables):
-                dfn = sm.diff(dfn, var, t_order[i])
-            if self.is_np_set_expr_fn:
-                dfn = sm.lambdify(self.variables, dfn, 'numpy')
-            self.dico_derivation[t_order] = dfn
-            return dfn
+        self.dico_order_to_index.update(
+            {(a, b): self.max_index+i for i, (a, b) in enumerate(l_orders)})
+
+        if self.is_np_set_expr_fn:
+            self.reduced_tab_diff = sm.lambdify(
+                variables, [self.tab_diff[a][b] for (a, b) in l_orders], 'numpy')
+            return self.reduced_tab_diff
+
+        self.reduced_tab_diff = [self.tab_diff[a][b] for (a, b) in l_orders]
+        return self.reduced_tab_diff
 
     def evaluate(self, X):
         """
         Returns the value of F at coordinates X
         - X: vector of coordinates of a point in 2D. X.shape == (2,) or len(X) == 2
+
+        remark: to see if np could take array instead
         """
         if self.is_np_set_expr_fn:
             return _eval_polynome_numpy(self.expr, X[0], X[1])
         else:
             return _eval_polynome(self.expr, X[0], X[1])
 
-    def evaluate_derivative(self, X, t_order):
+    def evaluate_one_diff(self, t_order, X):
         """
         Returns the value of A at coordinates X
-        - X: vector of coordinates of a point in 2D. X.shape == (2,) or len(X) == 2
         - t_order: tuple of length len(self.variables) (==2) selecting the derivative
+        - X: vector of coordinates of a point in 2D. X.shape == (2,) or len(X) == 2
+
+        Remark: suboptimal with numpy
         """
-        if t_order in self.dico_derivation:
+        (a, b) = t_order
+        if t_order in self.dico_order_to_index:
             if self.is_np_set_expr_fn:
-                return _eval_polynome_numpy(self.dico_derivation[t_order], X[0], X[1])
-            return _eval_polynome(self.dico_derivation[t_order], X[0], X[1])
+                # suboptimal, can evaluate all of them at once with np => done here... and thrown away
+                return _eval_polynome_numpy(self.reduced_tab_diff, X[0], X[1])[self.dico_order_to_index[(a, b)]]
+            return _eval_polynome(self.reduced_tab_diff[self.dico_order_to_index[(a, b)]], X[0], X[1])
         else:
-            dfn = self.derivate(t_order)
+            dfn = self.dynamic_diff_2D([t_order])
             if self.is_np_set_expr_fn:
-                return _eval_polynome_numpy(dfn, X[0], X[1])
-            return _eval_polynome(dfn, X[0], X[1])
+                return _eval_polynome_numpy(dfn, X[0], X[1])[0]
+            return _eval_polynome(dfn[0], X[0], X[1])
 
 
 if __name__ == '__main__':
@@ -193,4 +210,12 @@ if __name__ == '__main__':
     # strfn = 'a_b_c'
     # f = strfn.split('_')
     # print(f)
-    print('ok')
+    coords = np.array([[0, 0], [0, 1], [1, 0], [1, 1], [2, 2]])
+    F = F2D(coords, 'xpy_np_real')
+    # print(F.expr(1/2,1/2))
+    for X in coords:
+        print(F.evaluate(X))
+    F.dynamic_diff_2D([(0, 1)])
+    print(F.dico_order_to_index)
+    for X in coords:
+        print(F.evaluate_one_diff((0, 1), X+np.array([1/8, 1/8])))
