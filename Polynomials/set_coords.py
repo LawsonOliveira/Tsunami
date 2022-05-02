93379c97ceb956fc668870a0723816583206c296
Julien Rosenberger <julien.rosenberger@student-cs.fr>
2022-05-02 10:10:11 +0200

    F_functions class F2D(...) incoporated in NN_test_functions* TODO: seek why NN does not learn with an instance of this class.

--- Polynomials/set_coords.py
+++ Polynomials/set_coords.py
@@ -4,7 +4,10 @@ file = Path(__file__).resolve()
 package_root_directory = file.parents[1]
 sys.path.append(str(package_root_directory))
 
+import matplotlib.pyplot as plt
 import numpy as np
+
+
 
 def _set_coords_circle(nnodes_max=20):
     coords = np.empty((nnodes_max, 2), dtype=np.float64)
@@ -52,3 +55,25 @@ def _set_coords_circle_bord_with_radius_interval(nnodes_max=20, rmin=1.0):
         coords[i, 0] = r*np.cos(aux)
         coords[i, 1] = r*np.sin(aux)
     return coords
+
+
+def _set_coords_rectangle(l, L, nnodes_max=20):
+    coords = []
+    elements = np.linspace(0, 2*l+2*L, nnodes_max, endpoint=False)
+    for el in elements:
+        if el < l:
+            coords.append([el, 0])
+        elif el < l+L:
+            coords.append([l, el - l])
+        elif el < 2*l + L:
+            coords.append([l-(el - (l+L)), L])
+        else:
+            coords.append([0, L-(el - (2*l+L))])
+    return np.array(coords)
+
+
+if __name__ == '__main__':
+    coords = _set_coords_rectangle(1, 1, nnodes_max=12)
+    print(coords.shape)
+    plt.plot(coords[:, 0], coords[:, 1])
+    plt.show()
